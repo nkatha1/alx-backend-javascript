5ef1f1e87bb052a0403bad1ddf6884b5cfc41450
nkatha1 <nkathapatience1@gmail.com>
2024-11-29 01:43:05 +0000

    files

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,23 @@
+/**
+ * Create a function named calculateNumber with two arguments (number) a and b
+ * The function should round a and b and return the sum of it
+ */
+
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return (Math.round(a) + Math.round(b));
+  }
+  if (type === 'SUBTRACT') {
+    return (Math.round(a) - Math.round(b));
+  }
+  else if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    } else {
+      return (Math.round(a) / Math.round(b));
+    }
+  }
+  return 0;
+};
+
+module.exports = calculateNumber;
